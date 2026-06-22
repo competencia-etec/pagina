882ff49bc76c7b0a030dbb95d72ccbcdb21cb00f
Ironowl1907 <facuguialtana7@gmail.com>
2026-06-22 10:32:25 -0300

    Working with db service

--- backend/backend/models/user.py
+++ backend/backend/models/user.py
@@ -1,21 +1,7 @@
-from pydantic import BaseModel
 
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: str | None = None
-
-
-class User(BaseModel):
+class User:
     username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
-
-
-class UserInDB(User):
+    full_name: str
+    email: str
     hashed_password: str
+    disabled: bool = False
