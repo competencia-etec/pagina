e3bf90a49f15d6ca93f0ff835eb9dacf492f0e83
Ironowl1907 <facuguialtana7@gmail.com>
2026-06-25 08:44:01 -0300

    Working on user creation

--- backend/backend/api/endpoints/user.py
+++ backend/backend/api/endpoints/user.py
@@ -1,8 +1,10 @@
-from backend.models.user import User
 from typing import Annotated
-from fastapi import Depends
+from fastapi import Depends, APIRouter
 
-from backend.services.user_service import get_current_active_user
+from backend.models.user import CreateUser, User
+from backend.services.user_service import get_current_active_user, create_user as service_create_user
+
+router = APIRouter()
 
 
 def add_endpoints(app):
@@ -11,3 +13,7 @@ def add_endpoints(app):
         current_user: Annotated[User, Depends(get_current_active_user)],
     ) -> User:
         return current_user
+
+    @app.post("/users/new/")
+    async def web_create_user(user: CreateUser) -> User:
+        return await service_create_user(user)
