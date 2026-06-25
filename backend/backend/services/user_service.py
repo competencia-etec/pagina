087320e34cc31fe59d9d7aefa0d4f9709b363fbc
Ironowl1907 <facuguialtana7@gmail.com>
2026-06-25 09:13:41 -0300

    Adding error propagation and duplicated user error

--- backend/backend/services/user_service.py
+++ backend/backend/services/user_service.py
@@ -1,32 +1,39 @@
-
 from typing import Annotated
-
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 import jwt
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 
 from backend.api.dependencies import hash_password, verify_password
+from backend.core import logger
 from backend.core.config import EnvirometConfig
 from backend.models.user import CreateUser, TokenData, User
 from backend.services.database import DatabaseConnection
 
-
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+db = DatabaseConnection()
 
 
 def get_user(username: str):
-    db = DatabaseConnection()
-
-    return db.get_user(username)
+    try:
+        return db.get_user(username)
+    except SQLAlchemyError as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Database service unavailable"
+        )
 
 
 def authenticate_user(username: str, password: str):
     user = get_user(username)
-    if (user is None):
+    if user is None:
         return False
+
     # HACK: Remove false password
-    if (password == "fake_password"):
+    if password == "fake_password":
         return user
+
     if not verify_password(password, user.hashed_password):
         return False
     return user
@@ -40,22 +47,17 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         headers={"WWW-Authenticate": "Bearer"},
     )
 
-    internal_exception = HTTPException(
-        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-        detail="Error procesing the response",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
     try:
-        payload = jwt.decode(token, env.get_config_var(
-            "SECRET_KEY"), env.get_config_var("ALGORITHM"))
-        username = payload.get("sub")
-        if username is None:
-            raise credentials_exception
+        payload = jwt.decode(
+            token,
+            env.get_config_var("SECRET_KEY"),
+            algorithms=[env.get_config_var("ALGORITHM")]
+        )
+        username: str = payload.get("sub")
         token_data = TokenData(username=username)
-        if token_data is None:
-            raise internal_exception
     except jwt.InvalidTokenError:
         raise credentials_exception
+
     user = get_user(token_data.username)
     if user is None:
         raise credentials_exception
@@ -66,26 +68,36 @@ async def get_current_active_user(
     current_user: Annotated[User, Depends(get_current_user)],
 ):
     if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user")
     return current_user
 
 
 async def create_user(creating_user: CreateUser) -> User:
-    db = DatabaseConnection()
-
     hashed_password = hash_password(creating_user.unhashed_password)
 
-    db.add_user(
-        username=creating_user.username,
-        full_name=creating_user.full_name,
-        email=creating_user.email,
-        hashed_password=hashed_password,
-        disabled=creating_user.disabled
-    )
+    try:
+        db.add_user(
+            username=creating_user.username,
+            full_name=creating_user.full_name,
+            email=creating_user.email,
+            hashed_password=hashed_password,
+            disabled=creating_user.disabled
+        )
+    except IntegrityError as e:
+        logger.logger.error(f"Integrity error (Duplicate User/Email): {e}")
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User or email already exists",
+        )
+    except SQLAlchemyError as e:
+        logger.logger.error(f"SQL ERROR: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error processing the response",
+        )
 
-    user = User(
+    return User(
         **creating_user.model_dump(exclude={"unhashed_password"}),
         hashed_password=hashed_password
     )
-
-    return user
